bc963ef2ab2d5c7a69ad9435d1c5e9762f0be21b
Alex Ramos <jramosneto01@mynbcc.ca>
2024-02-29 10:11:35 -0400

    Finished home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 import '@styles/globals.css'
 
 export const metadata = {
@@ -9,12 +11,15 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
             <main className='app'>
+                <Nav />
                 {children}
             </main>
+            </Provider>
         </body>
 
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover and Share
-        <br className="max-md:hidden" />
+        <br className="max-sm:hidden" />
         <span className="orange_gradient text-center">
           AI-Powered prompts
         </span>
